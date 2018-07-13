737cebab08fbaf65c07d79b08d5e5ef6529c000e
Ryan Poe <baublet@gmail.com>
2018-07-13 15:03:19 -0400

    Add tests

---
+++ api/priv/repo/migrations/20180713184026_create_todos.exs
@@ -0,0 +1,14 @@
+defmodule Api.Repo.Migrations.CreateTodos do
+  use Ecto.Migration
+
+  def change do
+    create table(:todos) do
+      add :user, :string
+      add :todo, :string
+      add :completed, :boolean, default: false, null: false
+
+      timestamps()
+    end
+
+  end
+end
--- api/test/support/model_case.ex
+++ api/test/support/model_case.ex
@@ -1,17 +1,4 @@
 defmodule Api.ModelCase do
-  @moduledoc """
-  This module defines the test case to be used by
-  model tests.
-
-  You may define functions here to be used as helpers in
-  your model tests. See `errors_on/2`'s definition as reference.
-
-  Finally, if the test case interacts with the database,
-  it cannot be async. For this reason, every test runs
-  inside a transaction which is reset at the beginning
-  of the test unless the test case is marked as async.
-  """
-
   use ExUnit.CaseTemplate
 
   using do
@@ -35,21 +22,6 @@ defmodule Api.ModelCase do
     :ok
   end
 
-  @doc """
-  Helper for returning list of errors in a struct when given certain data.
-
-  ## Examples
-
-  Given a User schema that lists `:name` as a required field and validates
-  `:password` to be safe, it would return:
-
-      iex> errors_on(%User{}, %{password: "password"})
-      [password: "is unsafe", name: "is blank"]
-
-  You could then write your assertion like:
-
-      assert {:password, "is unsafe"} in errors_on(%User{}, %{password: "password"})
-  """
   def errors_on(struct, data) do
     struct.__struct__.changeset(struct, data)
     |> Ecto.Changeset.traverse_errors(&Api.ErrorHelpers.translate_error/1)
